50b3b3a3da0275f8c2b28c365a6071ec5042be7e
Luiz Renato Délio da Costa <luizdelio123@gmail.com>
2026-05-01 23:01:36 -0300

    Add Aluno class with attributes and methods

---
+++ Aluno.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.br.ifrn.sistemaEscolar.model;
+
+import edu.br.ifrn.sistemaEscolar.model.*;
+import java.util.ArrayList;
+
+
+
+public class Aluno {
+    
+    
+    
+    private String nome;
+    private int idade;
+    private String matricula;
+
+    public Aluno(String nome, int idade, String matricula) {
+        this.nome = nome;
+        this.idade = idade;
+        this.matricula = matricula;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
+    
+}
